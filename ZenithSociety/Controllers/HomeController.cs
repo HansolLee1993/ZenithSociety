f68b47317638c7b617c54763161fe07f134488ed
Morris Arroyo <arroyomorris28@gmail.com>
2017-10-12 22:08:21 -0700

    Added Week Events table to homepage

--- ZenithSociety/Controllers/HomeController.cs
+++ ZenithSociety/Controllers/HomeController.cs
@@ -1,16 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ZenithSociety.Models;
 
 namespace ZenithSociety.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
-            return View();
+            var events = db.Events.Include(e => e.ActivityCategory).ToList();
+            DateTime first = DateTime.Today.AddDays(1 - ((int)DateTime.Today.DayOfWeek));
+            // 1 offset for start in monday     
+            DateTime last = first.AddDays(7);
+            List<Event> eventsOfWeek = new List<Event>();
+
+            foreach (Event eve in events)
+            {
+                if (eve.EventFromDate.Date.CompareTo(first.Date) >= 0 && eve.EventFromDate.Date.CompareTo(last.Date) < 0)
+                {
+                    eventsOfWeek.Add(eve);
+                }
+            }
+            return View(eventsOfWeek.OrderBy(e => e.EventFromDate).ToList());
+
+            //return View();
         }
 
         public ActionResult About()
@@ -27,6 +46,6 @@ namespace ZenithSociety.Controllers
             return View();
         }
 
-      
+
     }
 }
